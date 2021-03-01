7a9fd317abbe80fd0d2cc9947104f4022812f7f6
LidiaParral <72860314+LidiaParral@users.noreply.github.com>
2021-03-01 02:22:26 +0100

    Calculadora2

--- Calculadora2/ViewController.swift
+++ Calculadora2/ViewController.swift
@@ -10,11 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var numero1: UITextField!
+    
+    @IBOutlet weak var numero2: UITextField!
+    
+    @IBOutlet weak var resultado: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func suma(_ sender: Any) {
+        let num = Double(numero1.text!)
+        let calculo = Double(numero2.text!)
+        let suma = num! + calculo!;
+        resultado.text = String(suma)
+    }
+    
+    @IBAction func resta(_ sender: Any) {
+        let num = Double(numero1.text!)
+        let calculo = Double(numero2.text!)
+        let suma = num! - calculo!;
+        resultado.text = String(suma)
+    }
+    @IBAction func multiplicacion(_ sender: Any) {
+        let num = Double(numero1.text!)
+        let calculo = Double(numero2.text!)
+        let suma = num! * calculo!;
+        resultado.text = String(suma)
+    }
+    
+    @IBAction func division(_ sender: Any) {
+        let num = Double(numero1.text!)
+        let calculo = Double(numero2.text!)
+        let suma = num! / calculo!;
+        resultado.text = String(suma)
+    }
 }
 
